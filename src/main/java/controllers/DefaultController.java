d6ac39549c0a24ec20838e208309e9c7bbafd558
pavserger@gmai.com <pavserger@gmail.com>
2024-08-23 15:54:08 +0300

    230824 no work

---
+++ src/main/java/controllers/DefaultController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+    /**
+     * Метод формирует страницу из HTML-файла index.html,
+     * который находится в папке resources/templates.
+     * Это делает библиотека Thymeleaf.
+     */
+
+
+       //@Autowired
+
+
+    @RequestMapping("/")
+    public String index() {
+
+
+        return "index";
+    }
+
+
+
+}
